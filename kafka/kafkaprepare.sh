7f57c376822235fe28d19619e214400935992dc1
Mitsutoshi Kiuchi <m-kiuchi@creationline.com>
2017-03-05 23:50:37 +0900

    [FIX] kafka

--- kafka/kafkaprepare.sh
+++ kafka/kafkaprepare.sh
@@ -6,11 +6,17 @@ METADATA=$METADATA_HOST/$METADATA_VERSION
 function metadata { echo $(curl -s $METADATA/$1); }
 ###############################################################################
 
-MYINDEX=$(metadata self/service/create_index)
+MYINDEX=$(metadata self/container/create_index)
 MYPORT=$(expr 9091 + $MYINDEX)
 MYHOST=$(hostname -i)
+ZK_STRING=$(cat /tmp/ZK_STRING)
+ADDUSER=$(cat /tmp/ADDUSER)
 
 cat /opt/kafka/config/server.properties |sed -e "s/broker.id=0/broker.id=${MYINDEX}/" > 1
 cat 1 | sed -e "s/#listeners=PLAINTEXT:\/\/:9092/listeners=PLAINTEXT:\/\/:${MYPORT}/" > 2
-mv 2 /opt/kafka/config/server.properties
-rm -f 1
+cat 2 | sed -e "s/zookeeper.connect=localhost:2181/zookeeper.connect=${ZK_STRING}/" > 3
+mv 3 /opt/kafka/config/server.properties
+rm -f 1 2
+
+echo 'alias kafka-topics="sudo /opt/kafka/bin/kafka-topics.sh --zookeeper '${ZK_STRING}'"' >> /home/${ADDUSER}/.bashrc
+echo 'alias kafka-preferred-replica-election="sudo /opt/kafka/bin/kafka-preferred-replica-election.sh --zookeeper '${ZK_STRING}'"' >> /home/${ADDUSER}/.bashrc
